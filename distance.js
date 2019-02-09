3d80e4abf453055fdd8cca15b200e76666e623e2
Anvesh <anveshrao1616@gmail.com>
2019-02-08 23:50:27 -0600

    FINAL
    Signed-off-by: Anvesh <anveshrao1616@gmail.com>

--- distance.js
+++ distance.js
@@ -2,5 +2,5 @@ function Calculate()
 {
         num1 = parseInt(document.getElementById("speed").value);
         num2 = parseInt(document.getElementById("time").value);
-        document.getElementById("result").innerHTML = num1 * num2;
+        document.getElementById("result").innerHTML = num1 * num2+ " km";
 }
