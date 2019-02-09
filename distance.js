9b3c1f2e047f2c186c708668f3920e8f95aaf391
Anvesh <anveshrao1616@gmail.com>
2019-02-08 23:19:14 -0600

    Final

---
+++ distance.js
@@ -0,0 +1,6 @@
+function Calculate()
+{
+        num1 = parseInt(document.getElementById("speed").value);
+        num2 = parseInt(document.getElementById("time").value);
+        document.getElementById("result").innerHTML = num1 * num2;
+}
