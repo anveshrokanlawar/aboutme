d01039b195897bffbb13545a46cf2eb627338358
Anvesh <anveshrao1616@gmail.com>
2019-02-22 03:08:10 -0600

    Assignment
    Signed-off-by: Anvesh <anveshrao1616@gmail.com>

--- distance.js
+++ distance.js
@@ -1,5 +1,13 @@
 function Calculate() {
-        num1 = parseInt(document.getElementById("speed").value);
-        num2 = parseInt(document.getElementById("time").value);
+       const num1 = parseInt(document.getElementById("speed").value);
+       const num2 = parseInt(document.getElementById("time").value);
+       if(num1>=0&&num2>=0)
+       {
         document.getElementById("result").innerHTML = num1 * num2 + " km";
+       }
+       else
+       {
+        alert("Values cannot be negative");
+       }
+        
 }
