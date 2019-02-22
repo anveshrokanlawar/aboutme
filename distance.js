298fe5841a75c1c1b160de11bc3e34d7b257ad09
Anvesh <anveshrao1616@gmail.com>
2019-02-22 16:48:17 -0600

    Final

--- distance.js
+++ distance.js
@@ -1,13 +1,14 @@
-function Calculate() {
+function Calculate(speed,time) {
        const num1 = parseInt(document.getElementById("speed").value);
        const num2 = parseInt(document.getElementById("time").value);
        if(num1>=0&&num2>=0)
        {
-        document.getElementById("result").innerHTML = num1 * num2 + " km";
+        const num3= num1*num2;
+        document.getElementById("result").innerHTML = num3+ " km";
        }
        else
        {
-        alert("Values cannot be negative");
+        alert("Invalid input values");
        }
         
 }
