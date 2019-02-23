936fa9793b160107af9dd0f1aea5d25e86351f1d
Anvesh <anveshrao1616@gmail.com>
2019-02-22 22:19:35 -0600

    Final

--- distance.js
+++ distance.js
@@ -1,14 +1,12 @@
-function Calculate(speed,time) {
-       const num1 = parseInt(document.getElementById("speed").value);
-       const num2 = parseInt(document.getElementById("time").value);
-       if(num1>=0&&num2>=0)
-       {
-        const num3= num1*num2;
-        document.getElementById("result").innerHTML = num3+ " km";
+function Calculate(speed, time) {
+       localStorage.setItem("num1",parseInt(document.getElementById("speed").value));
+       localStorage.setItem("num2",parseInt(document.getElementById("time").value));
+       if (localStorage.getItem("num1") >= 0 && localStorage.getItem("num2") >= 0) {
+              localStorage.setItem("num3", localStorage.getItem("num1")  * localStorage.getItem("num2")) ;
+              document.getElementById("result").innerHTML = localStorage.getItem("num3") + " km";
        }
-       else
-       {
-        alert("Invalid input values");
+       else {
+              alert("Invalid input values");
+              throw Error('Values cannot be negative');
        }
-        
 }
