95f1a982f8822254cf60ddf06216035d2769bef2
Anvesh <anveshrao1616@gmail.com>
2019-02-24 01:13:06 -0600

    final

--- distance.js
+++ distance.js
@@ -1,8 +1,8 @@
 function Calculate(speed, time) {
-       localStorage.setItem("num1",parseInt(document.getElementById("speed").value));
-       localStorage.setItem("num2",parseInt(document.getElementById("time").value));
+       localStorage.setItem("num1", parseInt(document.getElementById("speed").value));
+       localStorage.setItem("num2", parseInt(document.getElementById("time").value));
        if (localStorage.getItem("num1") >= 0 && localStorage.getItem("num2") >= 0) {
-              localStorage.setItem("num3", localStorage.getItem("num1")  * localStorage.getItem("num2")) ;
+              localStorage.setItem("num3", localStorage.getItem("num1") * localStorage.getItem("num2"));
               document.getElementById("result").innerHTML = localStorage.getItem("num3") + " km";
        }
        else {
@@ -10,3 +10,20 @@ function Calculate(speed, time) {
               throw Error('Values cannot be negative');
        }
 }
+function loadDoc() {
+       var xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange = function () {
+              if (this.readyState == 4 && this.status == 200) {
+                     document.getElementById("demo").innerHTML =
+                            this.responseText;
+              }
+       };
+       xhttp.open("GET", "info.txt", true);
+       xhttp.send();
+}
+
+$(document).ready(function () {
+       $("content").mouseout(function () {
+              $("demo").hide();
+       });
+});
